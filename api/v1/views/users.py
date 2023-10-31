33b9e2ddfe2245ca8ce1cc30d04a71c4a2f29621
‘laghdaf <laghdaf.benalla@gmail.com>
2023-10-31 11:32:08 +0100

    Done

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,76 +1,68 @@
 #!/usr/bin/python3
-""" state doc"""
+"""Users view"""
 
-from models.base_model import BaseModel
-from models import storage
-from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
-from models.state import State
+from flask import abort, jsonify, make_response, request
+from models import storage
 from models.user import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
-def all_users():
-    """doc fuction"""
-    users_list = []
-    users = storage.all(User).values()
-    for user in users:
-        users_list.append(user.to_dict())
-    return jsonify(users_list)
+def get_users():
+    """Fetch user information"""
+    users = []
+    for user in storage.all("User").values():
+        users.append(user.to_dict())
+    return jsonify(users)
 
 
-@app_views.route('users/<user_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/users/<string:user_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_user(user_id):
-    """get users"""
-    user = storage.get(User, user_id)
+    """Fetch user information for specified user"""
+    user = storage.get("User", user_id)
     if user is None:
         abort(404)
-    else:
-        return jsonify(user.to_dict())
+    return jsonify(user.to_dict())
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'],
+@app_views.route('/users/<string:user_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_user(user_id):
-    """ doc delete"""
-    user = storage.get(User, user_id)
+    """Delete a user based on its user_id"""
+    user = storage.get("User", user_id)
     if user is None:
         abort(404)
-    else:
-        storage.delete(user)
-        storage.save()
-        return jsonify({}), 200
+    user.delete()
+    storage.save()
+    return (jsonify({}))
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
-def create_user():
-    """doc create"""
-    request_data = request.get_json()
-    if not request_data:
-        abort(400, 'Not a JSON')
-    if 'email' not in request_data:
-        abort(400, 'Missing email')
-    if 'password' not in request_data:
-        abort(400, 'Missing password')
-    new_user = User(**request_data)
-    storage.new(new_user)
-    storage.save()
-    return jsonify(new_user.to_dict()), 201
+def post_user():
+    """Create a new user"""
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'email' not in request.get_json():
+        return make_response(jsonify({'error': 'Missing email'}), 400)
+    if 'password' not in request.get_json():
+        return make_response(jsonify({'error': 'Missing password'}), 400)
+    user = User(**request.get_json())
+    user.save()
+    return make_response(jsonify(user.to_dict()), 201)
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-def update_user(user_id):
-    """doc update"""
-    put_req = request.get_json()
-    if not put_req:
-        abort(400, "Not a JSON")
-    user = storage.get(User, user_id)
-    if not user:
+@app_views.route('/users/<string:user_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_user(user_id):
+    """Update a user"""
+    user = storage.get("User", user_id)
+    if user is None:
         abort(404)
-    ignore_keys = ['id', 'email', 'created_at', 'updated_at']
-
-    for key, value in put_req.items():
-        if key not in ignore_keys:
-            setattr(user, key, value)
-    storage.save()
-    return make_response(user.to_dict(), 200)
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    for attr, val in request.get_json().items():
+        if attr not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, attr, val)
+    user.save()
+    return jsonify(user.to_dict())
